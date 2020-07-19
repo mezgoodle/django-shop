8eeb301aa6af4213aab09b8b6528784646301f71
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-19 17:49:50 +0300

    Update cart.html

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -24,5 +24,12 @@ def cart(request):
 
 def checkout(request):
     template_name = 'store/checkout.html'
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, template_name, context)
