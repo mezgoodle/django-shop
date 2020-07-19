37526e97bebd21e96d8c2d54b3ca42102d00dabf
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-19 11:48:28 +0300

    Install pillow, make migrations, add images

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -123,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/store/img')
