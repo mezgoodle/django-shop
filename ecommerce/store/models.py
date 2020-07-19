1e6f9a72f517c36783921fad862a1af64195f8ed
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-19 13:16:25 +0300

    Render total values

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -46,6 +46,18 @@ class Order(models.Model):
     def __str__(self) -> str:
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
     class Meta:
         ordering = ['date_orderd']
         verbose_name = 'Order'
@@ -60,6 +72,11 @@ class OrderItem(models.Model):
 
     def __str__(self) -> str:
         return str(self.product)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
     class Meta:
         ordering = ['date_added']
