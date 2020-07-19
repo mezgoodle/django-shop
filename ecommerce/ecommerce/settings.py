c743ea5dde43fe3f35cc21d77a663a3a1fd6c664
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-19 11:52:04 +0300

    Set urls for images

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -123,4 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/store/img')
