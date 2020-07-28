05612de2f5d09e1e0ebd9004cf3277196c2d3e89
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-28 13:18:17 +0300

    Add/remove item

--- ecommerce/static/store/js/cart.js
+++ ecommerce/static/store/js/cart.js
@@ -5,11 +5,35 @@ for (let i = 0; i < updateBtns.length; i++) {
     const productId = this.dataset.product;
     const action = this.dataset.action;
     if (user === 'AnonymousUser') {
-      console.log('Not logged in');
+      addCookieItem(productId, action);
     } else {
       updateUserOrder(productId, action);
     }
   });
+}
+
+const addCookieItem = (productId, action) => {
+  console.log('Not logged in');
+
+  if (action === 'add') {
+    if (!cart[productId]) {
+      cart[productId] = {'quantity': 1};
+    } else {
+      cart[productId]['quantity'] += 1;
+    }
+  }
+
+  if (action === 'remove') {
+    cart[productId]['quantity'] -= 1;
+
+    if (cart[productId]['quantity'] <= 0) {
+      console.log('Remove item');
+      delete cart[productId];
+    }
+  }
+  console.log({cart});
+  document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/";
+  location.reload();
 }
 
 const updateUserOrder = (productId, action) => {
