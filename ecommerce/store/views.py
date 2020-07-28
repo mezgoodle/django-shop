d64a7f11236f2479684c104f29481f8945867557
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-28 17:55:56 +0300

    Make order for not auth users

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
-from .utils import cartData, cookieCart
+from .utils import cartData, cookieCart, guestOrder
 import json
 import datetime
 
@@ -69,33 +69,7 @@ def processOrder(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
     else:
-        print('User is not logged in')
-
-        print('COOKIES:', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
-
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(
-            email=email,
-        )
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(
-            customer=customer,
-            complete=False,
-        )
-
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity'],
-            )
+        customer, order = guestOrder(request, data)
     
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
