8eb18ed5b07f3c727b1dbb535f90dbb02a06b2d7
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-30 11:35:02 +0300

    Fix lint errors

--- ecommerce/static/store/js/cart.js
+++ ecommerce/static/store/js/cart.js
@@ -1,44 +1,41 @@
-let updateBtns = document.getElementsByClassName('update-cart')
+const updateBtns = document.getElementsByClassName('update-cart')
 
 for (let i = 0; i < updateBtns.length; i++) {
-  updateBtns[i].addEventListener('click', function(){
-    const productId = this.dataset.product;
-    const action = this.dataset.action;
+  updateBtns[i].addEventListener('click', function () {
+    const productId = this.dataset.product
+    const action = this.dataset.action
     if (user === 'AnonymousUser') {
-      addCookieItem(productId, action);
+      addCookieItem(productId, action)
     } else {
-      updateUserOrder(productId, action);
+      updateUserOrder(productId, action)
     }
-  });
+  })
 }
 
 const addCookieItem = (productId, action) => {
-  console.log('Not logged in');
-
   if (action === 'add') {
     if (!cart[productId]) {
-      cart[productId] = {'quantity': 1};
+      cart[productId] = { 'quantity': 1 }
     } else {
-      cart[productId]['quantity'] += 1;
+      cart[productId]['quantity'] += 1
     }
   }
 
   if (action === 'remove') {
-    cart[productId]['quantity'] -= 1;
+    cart[productId]['quantity'] -= 1
 
     if (cart[productId]['quantity'] <= 0) {
-      console.log('Remove item');
-      delete cart[productId];
+      console.log('Remove item')
+      delete cart[productId]
     }
   }
-  console.log({cart});
-  document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/";
-  location.reload();
+  console.log({ cart })
+  document.cookie = 'cart=' + JSON.stringify(cart) + ';domain=;path=/'
+  location.reload()
 }
 
 const updateUserOrder = (productId, action) => {
-  console.log('User is logged in');
-  const url = '/update_item/';
+  const url = '/update_item/'
   fetch(url, {
     method: 'POST',
     headers: {
@@ -50,11 +47,11 @@ const updateUserOrder = (productId, action) => {
       action,
     }),
   })
-  .then((res) => {
-    return res.json()
-  })
-  .then((data) => {
-    console.log({data})
-    location.reload();
-  })
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      console.log({ data })
+      location.reload()
+    })
 }
